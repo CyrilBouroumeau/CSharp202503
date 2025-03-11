1de9ced94418ee3617c6a37ef74fc6fc5c0b79c9
CyrilBouroumeau <cyril.bouroumeau@gmail.com>
2025-03-11 21:52:28 +0100

    V1 switch Csharp

--- CSharp202503/Program.cs
+++ CSharp202503/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("donne moi ton mdp à tester");
+string reponse = Console.ReadLine();
+int testmdpok = reponse.Length;
+
+if (testmdpok >= 8)
+{
+    Console.WriteLine("ton mdp est robuste: "+ reponse);
+}
+else if (testmdpok == 0)
+{
+    Console.WriteLine("Vous n'avez pas saisi de mdp");
+}
+else
+{
+    Console.WriteLine("ton mdp : " + reponse + "n'est pas robuste");
+}
+
+
+string continuer = "O";
+while (continuer == "O")
+{
+    Console.WriteLine("Donne moi un numéro de département");
+    string departement = Console.ReadLine();
+    int departementint = Convert.ToInt16(departement);
+    switch (departementint)
+{
+    case 1:
+        Console.WriteLine("AIN");
+        break;
+    case 2:
+        Console.WriteLine("ALLIER");
+        break;
+    default:
+        Console.WriteLine("Departement inconnu");
+        break;
+}
+    Console.WriteLine("continuer ? (O/N): ");
+    continuer = Console.ReadLine();
+}
